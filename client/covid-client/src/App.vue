84a06286451d42870adf64c53790b888f9cd5b90
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-03-23 15:51:06 +0700

    commit sebelum break

---
+++ client/covid-client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div id="nav" v-if="$store.state.isLogin">
+      <router-link to="/countries">Countries</router-link> |
+      <router-link to="/report">Profile</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+
+export default {
+  
+}
+</script>
